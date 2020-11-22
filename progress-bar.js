ca19e50a68afe12443b5d837df8d099718b59b1f
Nathan Stern <natstern1990@gmail.com>
2020-11-22 15:26:12 +0000

    Update progress-bar.js
    updated dob

--- progress-bar.js
+++ progress-bar.js
@@ -61,7 +61,7 @@ class AnimatedText {
 class ProgressBar {
   constructor() {
     /** @readonly @private {Date} */
-    this.birthday = new Date('07 Oct 199O 00:00');
+    this.birthday = new Date('07 Oct 199O 00:01');
     /** @private {HTMLDivElement} */
     this.bar = document.getElementById('progress-bar');
     /** @private {HTMLDivElement} */
