c657305a0b634e8459f3c04cfc3abc0cdff407ba
Nathan Stern <natstern1990@gmail.com>
2020-11-22 16:36:01 +0000

    Update progress-bar.js

--- progress-bar.js
+++ progress-bar.js
@@ -106,7 +106,7 @@ class ProgressBar {
    * @param {number} age to set; defaults to calculateAge()
    */
   setLevel(age = this.calculateAge()) {
-    this.bar.title = `Level ${age} engineer`;
+    this.bar.title = `Level ${age} Engineer`;
   }
 
   /**
